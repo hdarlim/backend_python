434873335f8a5b3201db6702c535459e0cf657a8
--global <lucascherez.cl@gmail.com>
2026-06-24 17:23:00 -0300

    port

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from app import app, db
+import os
 
 if __name__ == '__main__':
     db.inicializar()   # Cria as tabelas na primeira execução
-    app.run(debug=True)
+    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
